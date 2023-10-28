9483f8f91bec172046c1a12bae439225eb1620eb
Roland Takacs <rolandtakacs@Rolands-MBP.home>
2023-10-28 15:28:30 +0200

    fixes for changing the language, set different color to active menu item and minor changes

--- dog_grooming_app/static/js/dog_grooming_app.js
+++ dog_grooming_app/static/js/dog_grooming_app.js
@@ -1,5 +1,22 @@
 
 /**
+ * Sets the style of the active navigation menu item to active.
+ */
+$(document).ready(function() {
+    $('#topnav a').click(function(e) {
+        $('#topnav a').removeClass('active_menu');
+        $('#topnav button').removeClass('active_menu');
+        if (['nav_personal_data', 'nav_user_bookings', 'nav_admin_bookings'].includes($(this).attr('id'))) {
+            sessionStorage.setItem('active_menu', 'user_dropdown_button');
+        } else {
+            sessionStorage.setItem('active_menu', $(this).attr('id'));
+        }
+    });
+    const activeMenuId = sessionStorage.getItem('active_menu');
+    $('#' + activeMenuId).addClass('active_menu');
+});
+
+/**
  * Sets today's date by default to query free booking slots, on the Admin page.
  * Also sets the min and max value of the date input.
  */
